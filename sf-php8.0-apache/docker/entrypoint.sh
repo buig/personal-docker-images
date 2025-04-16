25402663d7b162c01b81c59a37c063d4422a6d5b
Antonio Calabuig Puigvert <antonio.calabuig@gmail.com>
2025-04-16 12:22:30 +0200

    [] - Configure timezone para contendedores que use la imagen

--- sf-php8.0-apache/docker/entrypoint.sh
+++ sf-php8.0-apache/docker/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Set timezone (a nivel de sistema y variable de entorno)
+ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+echo "Europe/Madrid" > /etc/timezone
+export TZ=Europe/Madrid
+
 # Inicia cron
 service cron start
 
